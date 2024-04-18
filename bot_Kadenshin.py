848391bb1a79f0445098210874441affea9cb025
Shin, Kaden <123702028+Petrograd-OktoberSong@users.noreply.github.com>
2024-04-17 21:44:40 -0700

    Create bot_Kadenshin.py

---
+++ bot_Kadenshin.py
@@ -0,0 +1,31 @@
+def choice(round_score, my_score, opponent_score):
+    import random
+    macron = pow(1-1/6, (100-my_score)/4)
+    d6 = [1,2,3,4,5,6]
+    scholz = pow(1-1/6, (100-(opponent_score+round_score))/4)
+    if my_score-opponent_score >= 10:
+        if round_score >= 20:
+            hold = 0
+        if round_score <= 20:
+            hold = 1
+    elif my_score-opponent_score >= 30:
+        if round_score >= 25:
+            hold = 0
+        if round_score <= 25:
+            hold = 1
+    else:
+        if round_score >= 16:
+            hold = 0
+        if round_score <= 16:
+            hold = 1
+    advice = random.choice(d6)
+    if advice <= 5:
+        if((round_score >= 15*(1+macron-scholz) and scholz < 0.5) or opponent_score+round_score >= 100):
+            return False
+        else:
+            return True
+    if advice == 6:
+        if hold == 0:
+            return False
+        if hold == 1:
+            return True
